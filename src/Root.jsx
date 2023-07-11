ed6503f7377b3f2e915cbb947b0343c317915eba
denis-larin-22 <denislarin2017@gmail.com>
2023-07-11 19:00:20 +0300

    Added tailwindcss

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,14 @@
 
-import { BcgContainer } from './components/BcgContainer';
+// import { BcgContainer } from './components/BcgContainer';
 import './styles/App.css';
 
 function Root() {
   return (
     <div className="App">
-      <BcgContainer />
+      <div className="container mx-auto w-[1000px] p-4">
+        <h1>Hello world</h1>
+      </div>
+      {/* <BcgContainer /> */}
     </div>
   );
 }
