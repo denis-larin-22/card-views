5173ddcb1e6f24524725aca6d8ff7da540ec91e2
denis-larin-22 <denislarin2017@gmail.com>
2023-07-18 22:41:40 +0300

    Code refactoring

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,55 +1,68 @@
-import { Link } from "react-router-dom";
-import { getFromPublic } from "../_utils/getFromPublic"
-import { connect } from "react-redux";
+import { getFromPublic } from "../_utils/getFromPublic";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import { motion, AnimatePresence } from "framer-motion";
 
-const CardView = ({ isVisibleHandler, listThemes, cardTheme, isPlayingBcg, toggleBcg, isPlayingAudio, toggleAudio, upNext, setCurrent }) => {
+const CardView = ({
+    isVisibleHandler,
+    listThemes,
+    cardTheme,
+    isPlayingBcg,
+    toggleBcg,
+    isPlayingAudio,
+    toggleAudio,
+    upNext,
+    setCurrent
+}) => {
+    //Icons from public
     const assets = {
         iconClose: getFromPublic('/assets/images/card-icons/Close.svg'),
         iconHide: getFromPublic('/assets/images/card-icons/Hide.png'),
         iconPlay: getFromPublic('/assets/images/card-icons/Play.svg'),
         iconPause: getFromPublic('/assets/images/card-icons/Pause.png'),
     }
+    //Styles for card components
+    const navLinkStyles = (title) => (`${activeContent === title && 'btn-active'} px-[15px] py-[10px] md:px-[24px] md:py-[12px] rounded-[99px] ease-in duration-300 hover:bg-white-0.2 hover:text-white`);
+    const buttonsStyle = {
+        navButtons: 'p-[10px] md:p-[15px] bg-white-0.1 rounded-[99px] ease-in duration-300 hover:bg-white-0.6',
+        mainButtons: 'px-[32px] py-[18px] rounded-[18px] italic bg-white-0.2 ease-in duration-300 hover:bg-white-0.6 hover:text-black active:scale-90'
+    }
+    //Animation styles for card content
+    const cardContentAnim = {
+        hidden: { opacity: 0, x: '-50px' },
+        visible: { opacity: 1, x: 0 },
+    }
 
     const { title, text, description, previeImage } = cardTheme;
     const [activeContent, setActiveContent] = useState('Info');
 
-    const linkesStyles = (title) => {
-        return `${activeContent === title && 'btn-active'} px-[15px] py-[10px] md:px-[24px] md:py-[12px] rounded-[99px] ease-in duration-300 hover:bg-white-0.2 hover:text-white`
-    }
-
     return (
         <AnimatePresence>
-            <div className="relative max-w-[960px] p-[24px] flex flex-col gap-[21px]  text-white-0.6">
-                <div className="flex items-center justify-center">
-                    <div className="flex gap-x-[5px] absolute top-[15px] left-[15px] md:top-[24px] md:left-[24px] ">
-                        <Link
-                            to={'..'}
-                            className="p-[10px] md:p-[15px] bg-white-0.1 rounded-[99px] ease-in duration-300 hover:bg-white-0.6"
-                        >
+            <div className="relative p-[24px] flex flex-col gap-[21px]  text-white-0.6">
+
+                <header className="flex items-center justify-center">
+                    <div className="flex gap-x-[5px] absolute top-[15px] left-[15px] md:top-[24px] md:left-[24px]">
+                        <Link to={'..'} className={`${buttonsStyle.navButtons}`}>
                             <img src={assets.iconClose} alt="icon-close" />
                         </Link>
-                        <button
-                            className="p-[10px] md:p-[15px] bg-white-0.1 rounded-[99px] ease-in duration-300 hover:bg-white-0.6"
-                            onClick={isVisibleHandler}
-                        >
+                        <button className={`${buttonsStyle.navButtons}`} onClick={isVisibleHandler}>
                             <img src={assets.iconHide} alt="icon-hide" />
                         </button>
                     </div>
 
                     <nav className="relative flex gap-[3px] mt-[30px] md:mt-0 text-cardNav-sm md:text-cardNav-md">
-                        <button className={`${linkesStyles('Info')}`} onClick={() => setActiveContent('Info')}>Info</button>
-                        <button className={`${linkesStyles('Chapters')}`} onClick={() => setActiveContent('Chapters')}>Chapters</button>
-                        <button className="px-[15px] py-[10px] md:px-[24px] md:py-[12px] rounded-[99px] ease-in duration-300 hover:bg-white-0.2 hover:text-white" onClick={upNext}>Up Next</button>
+                        <button className={`${navLinkStyles('Info')}`} onClick={() => setActiveContent('Info')}>Info</button>
+                        <button className={`${navLinkStyles('Chapters')}`} onClick={() => setActiveContent('Chapters')}>Chapters</button>
+                        <button className={`${navLinkStyles(null)}`} onClick={upNext}>Up Next</button>
                     </nav>
-                </div>
+                </header>
 
                 {activeContent === 'Info' &&
-                    <motion.div
-                        className="flex flex-col md:flex-row items-center gap-[21px]"
-                        initial={{ opacity: 0, x: '-50px' }}
-                        animate={{ opacity: 1, x: 0 }}
+                    <motion.main
+                        className="flex flex-col max-w-[912px] md:min-h-[128px] md:flex-row items-center gap-[21px]"
+                        initial={cardContentAnim.hidden}
+                        animate={cardContentAnim.visible}
                         exit={{ opacity: 0, x: '50px' }}
                     >
                         <img src={previeImage} alt="card-img" className="rounded-[18px] max-w-[224px] max-h-[128px]" />
@@ -60,45 +73,45 @@ const CardView = ({ isVisibleHandler, listThemes, cardTheme, isPlayingBcg, toggl
                         </div>
                         <div className="min-w-[251px] flex flex-col gap-y-[12px] text-cardBtn text-white">
                             <button
-                                className="flex items-center gap-x-[12px] px-[32px] py-[18px] rounded-[18px] italic bg-white-0.2 ease-in duration-300 hover:bg-white-0.6 hover:text-black active:scale-90"
+                                className={`flex items-center gap-x-[12px] ${buttonsStyle.mainButtons}`}
                                 onClick={toggleAudio}
                             >
                                 <img src={isPlayingAudio ? assets.iconPause : assets.iconPlay} alt="icon-play" className="w-[15px] h-[16px]" />
                                 From Beginning
                             </button>
                             <button
-                                className="px-[32px] py-[18px] rounded-[18px] italic bg-white-0.2 ease-in duration-300 hover:bg-white-0.6 hover:text-black active:scale-90"
+                                className={buttonsStyle.mainButtons}
                                 onClick={toggleBcg}
                             >
                                 {isPlayingBcg ? 'Stop playing' : 'Go to Series'}
                             </button>
                         </div>
-                    </motion.div>}
+                    </motion.main>}
 
                 {activeContent === 'Chapters' &&
-                    <motion.div
+                    <motion.main
                         className="max-w-[912px] md:min-h-[128px] grid grid-cols-2 md:flex items-center gap-[21px]"
-                        initial={{ opacity: 0, x: '-50px' }}
-                        animate={{ opacity: 1, x: 0 }}
+                        initial={cardContentAnim.hidden}
+                        animate={cardContentAnim.visible}
                         exit={{ opacity: 0, x: '50px' }}
                     >
                         {listThemes.map((item, index) => {
                             if (item.title === title) return null;
                             return (
-                                <button key={index} className="w-full h-full rounded-[18px] overflow-hidden object-cover" onClick={() => setCurrent(item)}>
-                                    <img src={item.previeImage} alt="preview" className="" />
+                                <button key={index} className="w-full h-full rounded-[18px] overflow-hidden ease-in duration-300 hover:scale-105 hover:rotate-6 active:scale-90" onClick={() => setCurrent(item)}>
+                                    <img src={item.previeImage} alt="preview" className="min-h-[128px]" />
                                 </button>
                             )
                         })}
-                    </motion.div>}
+                    </motion.main>}
             </div>
         </AnimatePresence>
     )
 };
-
+// Redux
 const mapDispatchToProps = (dispatch) => ({
     upNext: () => dispatch({ type: 'UP_NEXT' }),
     setCurrent: (item) => dispatch({ type: 'SET_CURRENT', payload: item })
 })
 
-export const Card = connect(null, mapDispatchToProps)(CardView) 
+export const Card = connect(null, mapDispatchToProps)(CardView);
--- src/components/CardPage.jsx
+++ src/components/CardPage.jsx
@@ -1,15 +1,20 @@
-import ReactPlayer from 'react-player';
-import { useRef, useState } from 'react';
-import { Card } from './Card';
-import { connect } from 'react-redux';
 import { getFromPublic } from '../_utils/getFromPublic';
+import { useRef, useState } from 'react';
+import ReactPlayer from 'react-player';
+import { connect } from 'react-redux';
+import { Card } from './Card';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const CardPageView = ({ currentTheme, listThemes }) => {
+    //Icons
     const showIcon = getFromPublic('/assets/images/card-icons/Open.png');
-
+    //Values from current state
     const { bcgImage, bcgVideo, audio, ...cardTheme } = currentTheme;
-
+    //Animation styles for card content
+    const opacity = {
+        visible: 'opacity: 1',
+        hidden: 'opacity: 0'
+    }
     const [isVisibleCard, setIsVisibleCard] = useState(true)
     const [isPlayingBcg, setIsPlayingBcg] = useState(false);
     const [isPlayingAudio, setIsPlayingAudio] = useState(false);
@@ -21,75 +26,74 @@ const CardPageView = ({ currentTheme, listThemes }) => {
     };
 
     return (
-        <AnimatePresence >
-            <motion.div
-                className="relative w-screen h-screen overflow-hidden bg-black z-[10] origin-top-left"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.4 }}
-            >
-                <audio ref={audioRef} loop src={audio} className=''></audio>
-                <ReactPlayer
-                    url={bcgVideo}
-                    playing={isPlayingBcg}
-                    loop
-                    muted
-                    width="100%"
-                    height="100%"
-                    className='absolute top-0 left-0 z-[-1] video-background'
-                />
+        <motion.div
+            className="relative w-screen h-screen overflow-hidden bg-black z-[10] origin-top-left"
+            initial={opacity.hidden}
+            animate={opacity.visible}
+            exit={opacity.hidden}
+            transition={{ duration: 0.4 }}
+        >
+            <audio ref={audioRef} loop src={audio} className=''></audio>
+            <ReactPlayer
+                url={bcgVideo}
+                playing={isPlayingBcg}
+                loop
+                muted
+                width="100%"
+                height="100%"
+                className='absolute top-0 left-0 z-[-1] video-background'
+            />
+            <AnimatePresence>
+                {isPlayingBcg ? null : <motion.img
+                    src={bcgImage}
+                    alt="default-background"
+                    className="w-[100%] h-[100%] absolute top-0 left-0 z-[-1] object-cover"
+                    initial={opacity.hidden}
+                    animate={opacity.visible}
+                    exit={opacity.hidden}
+                />}
+            </AnimatePresence>
+
+            <main className="container flex items-center justify-center content">
                 <AnimatePresence>
-                    {isPlayingBcg ? null : <motion.img
-                        src={bcgImage}
-                        alt="default-background"
-                        className="w-[100%] h-[100%] absolute top-0 left-0 z-[-1] object-cover"
-                        initial={{ opacity: 0.5 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                    />}
+                    {isVisibleCard && <motion.div
+                        className="border-[2px] border-white-0.2 rounded-[32px] backdrop-blur-xl"
+                        initial={{ y: "100px", opacity: 0 }}
+                        animate={{ y: "0", opacity: 1 }}
+                        exit={{ y: "100px", opacity: 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        <Card
+                            isVisibleHandler={() => setIsVisibleCard(!isVisibleCard)}
+                            cardTheme={cardTheme}
+                            listThemes={listThemes}
+                            isPlayingAudio={isPlayingAudio}
+                            toggleAudio={togglePlayAudio}
+                            isPlayingBcg={isPlayingBcg}
+                            toggleBcg={() => setIsPlayingBcg(!isPlayingBcg)}
+                        />
+                    </motion.div>}
                 </AnimatePresence>
 
-                <main className="container flex items-center justify-center content">
-                    <AnimatePresence>
-                        {isVisibleCard && <motion.div
-                            className="border-[2px] border-white-0.2 rounded-[32px] backdrop-blur-xl"
-                            initial={{ y: "100px", opacity: 0 }}
-                            animate={{ y: "0", opacity: 1 }}
-                            exit={{ y: "100px", opacity: 0 }}
-                            transition={{ duration: 0.5 }}
-                        >
-                            <Card
-                                isVisibleHandler={() => setIsVisibleCard(!isVisibleCard)}
-                                cardTheme={cardTheme}
-                                listThemes={listThemes}
-                                isPlayingAudio={isPlayingAudio}
-                                toggleAudio={togglePlayAudio}
-                                isPlayingBcg={isPlayingBcg}
-                                toggleBcg={() => setIsPlayingBcg(!isPlayingBcg)}
-                            />
-                        </motion.div>}
-                    </AnimatePresence>
-                    {!isVisibleCard && <motion.button
-                        className='flex items-center gap-x-[5px] fixed right-0 p-[10px] md:p-[15px] bg-white-0.1 rounded-[99px] ease-in duration-300 hover:bg-white-0.6 hover:text-black active:scale-90'
-                        onClick={() => setIsVisibleCard(!isVisibleCard)}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        transition={{ duration: 1.5 }}
-                    >
-                        <img src={showIcon} alt="show-card" />
-                        Show card
-                    </motion.button>}
-                </main>
-            </motion.div>
-        </AnimatePresence >
+                {!isVisibleCard && <motion.button
+                    className='flex items-center gap-x-[5px] fixed right-0 p-[10px] md:p-[15px] bg-white-0.1 rounded-[99px] ease-in duration-300 hover:bg-white-0.6 hover:text-black active:scale-90'
+                    onClick={() => setIsVisibleCard(!isVisibleCard)}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 1.5 }}
+                >
+                    <img src={showIcon} alt="show-card" />
+                    Show card
+                </motion.button>}
+            </main>
+        </motion.div>
     )
 };
-
+//Redux
 const mapStateToProps = (state) => ({
     currentTheme: state.cardSetReducer.currentTheme,
     listThemes: state.cardSetReducer.listTheme,
-})
+});
 
 export const CardPage = connect(mapStateToProps)(CardPageView);
